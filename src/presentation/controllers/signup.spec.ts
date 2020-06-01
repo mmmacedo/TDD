8f0217b699c780053a07bdabb96a6b9227ba5143
mmmacedo <murilommacedo666@gmail.com>
2020-06-01 08:02:00 -0300

    refactor: fixing error class

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,6 @@
 // sut = System Under Test
 import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missingParamError'
 
 describe('SignUp Controller', () => {
   test('Should return an error if no name is provided', () => {
@@ -13,7 +14,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Should return an error if no email is provided', () => {
@@ -27,6 +28,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
