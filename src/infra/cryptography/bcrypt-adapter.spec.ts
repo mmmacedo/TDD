556802884bd6989c60e511f66815d99b9cb5e43b
mmmacedo <murilommacedo666@gmail.com>
2020-06-21 15:15:36 -0300

    test: ensure adapter throw when brypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -18,14 +18,19 @@ describe('Bcrypt adapter', () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
-
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hasn when success', async () => {
     const sut = makeSut()
     const result = await sut.encrypt('any_value')
-
     expect(result).toBe('mocked_hash')
+  })
+
+  test('Should throw if encrypter throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
