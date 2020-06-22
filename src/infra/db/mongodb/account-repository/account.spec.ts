bbf7ce7a90fe4cab56b67ea4f8fcfa0a782766c1
mmmacedo <murilommacedo666@gmail.com>
2020-06-22 08:09:05 -0300

    test: connect on db and add an account

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from '@/infra/db/mongodb/helper/mongo-helper'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository/account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an Account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_mail@email.com',
+      password: 'valid_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_mail@email.com')
+    expect(account.password).toBe('valid_password')
+  })
+})
