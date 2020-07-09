3a3b1b3809a74168f64327d0506d9bfc2a07bcd6
mmmacedo <murilommacedo666@gmail.com>
2020-07-09 07:57:58 -0300

    test: ensure cors is enable

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import app from '@/main/config/app'
+import request from 'supertest'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('access-allow-control-origin', '*')
+      .expect('access-allow-control-methods', '*')
+      .expect('access-allow-control-headers', '*')
+  })
+})
