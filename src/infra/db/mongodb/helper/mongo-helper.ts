d2b41c2749e7dd741a64e092e8041130d93463a1
mmmacedo <murilommacedo666@gmail.com>
2020-07-13 08:50:41 -0300

    refactor: alterados imports e conexao com mongo

--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -4,8 +4,9 @@ import { DefaultModel } from '@/domain/models/default-model'
 export const MongoHelper = {
   client: null as MongoClient,
 
-  async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(url, {
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
