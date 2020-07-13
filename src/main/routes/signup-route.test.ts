f6e3620dc0dc21e8c36f47970c4ea6805e347f0f
mmmacedo <murilommacedo666@gmail.com>
2020-07-13 07:54:14 -0300

    refactor: add connection with db on signup route test

--- src/main/routes/signup-route.test.ts
+++ src/main/routes/signup-route.test.ts
@@ -1,7 +1,21 @@
 import app from '@/main/config/app'
 import request from 'supertest'
+import { MongoHelper } from '@/infra/db/mongodb/helper/mongo-helper'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return 200 on success', async () => {
     await request(app)
       .post('/api/signup')
