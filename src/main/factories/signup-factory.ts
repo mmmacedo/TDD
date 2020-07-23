08508ee7483bc86a9b5d9ade92fd0eed2de8b314
mmmacedo <murilommacedo666@gmail.com>
2020-07-23 09:06:23 -0300

    feat: using decorator to log errors

--- src/main/factories/signup-factory.ts
+++ src/main/factories/signup-factory.ts
@@ -3,13 +3,16 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../../main/decorators/log-decorator'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignupController = (): SignUpController => {
+export const makeSignupController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
   const salt = 12
   const bCryper = new BcryptAdapter(salt)
   const accountMongoRepo = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bCryper, accountMongoRepo)
   const signUpController = new SignUpController(emailValidator, dbAddAccount)
-  return signUpController
+  const logSignUpController = new LogControllerDecorator(signUpController)
+  return logSignUpController
 }
